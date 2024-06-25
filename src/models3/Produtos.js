0912690ccc4c6adeab8934efaa90cc8b20740241
Andres Cristian <cristian.yapiticona@gmail.com>
2024-06-25 13:50:47 -0300

    Add files via upload

---
+++ src/models3/Produtos.js
@@ -0,0 +1,43 @@
+const { model, Schema } = require('mongoose')
+
+
+//vamos criar um obejto -> modelo para coleção
+//Importante:
+const produtosSchema = new Schema({
+  empresa: {
+    type: String
+  },
+  codigo: {
+    type: String
+  },
+  produto: {
+    type: String
+  },
+  estoque: {
+    type: String
+  },
+  valor: {
+    type: String
+  },
+  lucro: {
+    type: String
+  },
+  resultado: {
+    type: String
+  },
+  preco: {
+    type: String
+  },
+  site: {
+    type: String
+  },
+  imagemProduto: {
+    type: String
+  }
+
+
+
+})
+
+//exportar o Schema -> main 
+module.exports = model('Produtos', produtosSchema)
