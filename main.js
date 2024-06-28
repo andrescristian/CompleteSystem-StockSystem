95bf3553568bbe16362124065d5f14eb79fa21fa
Andres Cristian <cristian.yapiticona@gmail.com>
2024-06-28 16:38:44 -0300

    Update main.js

--- main.js
+++ main.js
@@ -4,22 +4,16 @@ const path = require('node:path')
 // ###################################################
 //importar fs para trabalhar com os arquivos de imagens
 const fs = require('fs')
-
-// importar o módulo do banco de dados
 const { conectar, desconectar } = require('./db')
 //importar o Schema (models)
 const Cadastros = require(`${__dirname}/src/models/Cadastros`)
 const Fornecedores = require(`${__dirname}/src/models2/Fornecedores`)
 const Produto = require(`${__dirname}/src/models3/Produtos`)
 
-//Importante!
-//janela principal
 let win
 const createWindow = () => {
-    //forçar o modo escuro na janela do sistema 
     nativeTheme.themeSource = 'dark'
-
-    win = new BrowserWindow({     //tirar a const e deixar só o win 
+    win = new BrowserWindow({
         width: 1280,
         height: 720,
         webPreferences: {
@@ -29,16 +23,11 @@ const createWindow = () => {
         },
         icon: `${__dirname}/src/public/img/logoaaa.png`
     })
-
-    //Carregar o menu personalizado
     const menuPersonalizado = Menu.buildFromTemplate(menuTemplate)
     Menu.setApplicationMenu(menuPersonalizado)
-
     win.loadFile(`${__dirname}/src/views/index.html`)
 }
-//Fim da janela principal
 
-//Janela de cadastro
 let winAbout
 const aboutWindow = () => {
     winAbout = new BrowserWindow({
@@ -56,11 +45,7 @@ const aboutWindow = () => {
     winAbout.loadFile(`${__dirname}/src/views/cadastro.html`)
 
 }
-//fim da janela de cadastro
 
-
-
-//Janela de renderização dos Cadastros
 const aboutWindow2 = () => {
     winAbout = new BrowserWindow({
         width: 1280,
@@ -71,16 +56,12 @@ const aboutWindow2 = () => {
             nodeIntegration: true,
             contextIsolation: false
         }
-
-
     })
     const menuPersonalizado = Menu.buildFromTemplate(menuTemplate)
     Menu.setApplicationMenu(menuPersonalizado)
     winAbout.loadFile(`${__dirname}/src/views/editar.html`)
 }
-//fim do editar cadastro
 
-//Janela Sobre
 let winAbout3
 const aboutWindow3 = () => {
     if (!winAbout3) {
@@ -93,20 +74,15 @@ const aboutWindow3 = () => {
                 nodeIntegration: true,
                 contextIsolation: false
             }
-
         })
     }
-    // Esconde o menu da janela
     winAbout3.setMenu(null)
-
     winAbout3.loadFile(`${__dirname}/src/public/img/logoaaa.png`)
     winAbout3.on('closed', () => {
         winAbout3 = null
     })
 }
-//Fim da janela sobre
 
-//Inicio da janela de cadastro de fornecedores
 let winFornecedores
 const fornecedoresWindow = () => {
     winFornecedores = new BrowserWindow({
@@ -124,9 +100,7 @@ const fornecedoresWindow = () => {
     winFornecedores.loadFile(`${__dirname}/src/views/fornecedores.html`)
 
 }
-//Fim da Janela de cadastro de fornecedores
 
-//Inicio da janela de renderização de fornecedores
 let winRenderizar
 const renderizarWindow = () => {
     winRenderizar = new BrowserWindow({
@@ -140,9 +114,7 @@ const renderizarWindow = () => {
             contextIsolation: false
         }
     })
-
     winRenderizar.loadFile(`${__dirname}/src/views/relatorio.html`)
-
 }
 
 let produtos
@@ -157,8 +129,6 @@ const telaProdutos = () => {
       nodeIntegration: true,
       contextIsolation: false,
     }
-
-
   })
   produtos.loadFile(`${__dirname}/src/views/produto.html`)
 }
@@ -174,22 +144,12 @@ const relatorioProd = () => {
       nodeIntegration: true,
       contextIsolation: false
     }
-
-
   })
   relatorioProdutos.loadFile(`${__dirname}/src/views/tabelaProdutos.html`)
 }
 
-//Fim da Janela de cadastro de fornecedores
-
-
-
 app.whenReady().then(() => {
-    createWindow() //criar a janela
-    //IMPORTANTE!!! Executar tambem a função novoArquivo() para criar o objeto file (mesmo comportamento do bloco de notas e editores de codigo)
-
-
-
+    createWindow()
     app.on('activate', () => {
         if (BrowserWindow.getAllWindows().length === 0) {
             createWindow()
@@ -203,22 +163,18 @@ app.on('window-all-closed', () => {
     }
 })
 
-//encerrar a conexão com o banco de dados antes do aplicativo for fechado
 app.on('before-quit', async () => {
     await desconectar()
 })
 
-//acrescentar este processo (correção de bug reload icone status) - passo 2 slide
 ipcMain.on('send-message', (event, message) => {
     console.log("<<<", message)
     statusConexao()
 })
 
-//status de conexão 
 const statusConexao = async () => {
     try {
         await conectar()
-        //enviar uma mensagem para a janela (renderer.js) informando o status da conexão e os erros caso ocorram
         win.webContents.send('db-status', "Banco de dados conectado")
     } catch (error) {
         win.webContents.send(`db-status', "Erro de conexão: ${error.message}`)
@@ -249,7 +205,6 @@ ipcMain.on('relatorio-produtos', () => {
     relatorioProd()
 })
 
-//Template do menu 
 const menuTemplate = [
     {
         label: 'Arquivo',
@@ -268,10 +223,9 @@ const menuTemplate = [
             },
             {
                 label: 'Sair',
-                accelerator: 'Alt+F4', //accelerator cria atalhos
+                accelerator: 'Alt+F4',
                 click: () => app.quit()
             }
-
         ]
     },
     {
@@ -345,8 +299,6 @@ const menuTemplate = [
                 label: 'Restaurar o zoom padrão',
                 role: 'resetZoom'
             }
-
-
         ]
     },
     {
@@ -355,18 +307,12 @@ const menuTemplate = [
             {
                 label: 'Sobre',
                 click: aboutWindow3
-
             }
         ]
     }
 ]
 
-
-//CRUD Create >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
 ipcMain.on('new-task', async (event, args) => {
-    console.log(args) // teste de recebimento
-    // Salvar no banco de dados os dados do formulario
-    // validação dos campos obrigatorios
     if (args.nome === "") {
         dialog.showMessageBox(winAbout, {
             type: "info",
@@ -383,9 +329,6 @@ ipcMain.on('new-task', async (event, args) => {
     } else {
         const novoCadastro = new Cadastros(args)
         await novoCadastro.save()
-
-        //enviar uma confirmação para o renderer(front-end) - passo 4
-        //passando a nova tarefa no formato JSON (Passo extra CRUD READ)
         event.reply('new-task-created', JSON.stringify(novoCadastro))
         dialog.showMessageBox(winAbout, {
             type: 'info',
@@ -398,30 +341,17 @@ ipcMain.on('new-task', async (event, args) => {
 })
 
 ipcMain.on('new-produto', async (event, args) => {
-    console.log(args) // teste de recebimento
-    // Salvar no banco de dados os dados do formulario
-    // validação dos campos obrigatorios
     try {
-      
-       // ###################################################
-          // se não existir um diretório chamado uploads, criar
           const uploadsDir = path.join(__dirname, 'uploads')
           if (!fs.existsSync(uploadsDir)) {
               fs.mkdirSync(uploadsDir)
           }
-  
-          // ###################################################
-          // gerar o nome do arquivo
           const fileName = `${Date.now()}_${path.basename(args.imagemProduto)}`
           console.log(fileName);
-          // ###################################################
-          // Obter o caminho de destino (armazenar em banco)
           const destination = path.join(uploadsDir, fileName)
           console.log(destination);
-          // ###################################################
-          // copiar o arquivo de imagem para pasta uploads
           fs.copyFileSync(args.imagemProduto, destination)
-  
+
           const novoProduto = new Produto({
             codigo: args.codigo,
             produto: args.produto,
@@ -432,8 +362,6 @@ ipcMain.on('new-produto', async (event, args) => {
             preco: args.lucro,
             imagemProduto: destination
         })
-  
-          // const novoProduto = new produtoModel(produto)
           dialog.showMessageBox(produtos, {
               type: 'info',
               title: 'CONEST',
@@ -457,46 +385,26 @@ ipcMain.on('new-produto', async (event, args) => {
       } else {
         const novoCadastro = new Produto(args)
         await novoCadastro.save()
-    
-        //enviar uma confirmação para o renderer(front-end) - passo 4
-        //passando a nova tarefa no formato JSON (Passo extra CRUD READ)
         event.reply('new-produto-created', JSON.stringify(novoCadastro))
       }
     } catch (error) {
-      
     }
-  
   })
  
-// ipcMain.on('new-task-refresh', async (event, args) => {
-//   event.reply('refresh-page', JSON.stringify(novoCadastro))
-// })
-
-//>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
-//CRUD READ
-//passo 2(slide) fazer uma busca no banco de dados de todas as tarefas pendentes
 ipcMain.on('get-tasks', async (event, args) => {
-    const cadastrosPendentes = await Cadastros.find() //.find faz a busca e como o "select no mysql"
-    console.log(cadastrosPendentes) //passo 2 fins didaticos (teste)
-    //passo 3(slide) enviar ao renderer(view) as tarefas pendentes
-    event.reply('pending-tasks', JSON.stringify(cadastrosPendentes))//JSON.stringify converte para o JSON
+    const cadastrosPendentes = await Cadastros.find()
+    console.log(cadastrosPendentes)
+    event.reply('pending-tasks', JSON.stringify(cadastrosPendentes))
 })
 
 ipcMain.on('get-produto', async (event, args) => {
-    const cadastrosPendentes = await Produto.find() //.find faz a busca e como o "select no mysql"
-    console.log(cadastrosPendentes) //passo 2 fins didaticos (teste)
-    //passo 3(slide) enviar ao renderer(view) as tarefas pendentes
-    event.reply('pending-produto', JSON.stringify(cadastrosPendentes))//JSON.stringify converte para o JSON
+    const cadastrosPendentes = await Produto.find()
+    console.log(cadastrosPendentes)
+    event.reply('pending-produto', JSON.stringify(cadastrosPendentes))
   })
 
-
-
-//>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
-//CRUD Delete - excluir os dados do banco
-//passo 2(slide) receber o pedido do renderer para excluir uma tarefa do banco de dados
 ipcMain.on('delete-task', async (event, args) => {
-    console.log(args) // teste de recebimentodo id (passo 2 slide)
-    //exibir uma caixa de dialogo para confirma a exclusão
+    console.log(args)
     const { response } = await dialog.showMessageBox(win, {
         type: 'warning',
         buttons: ['Cancelar', 'Excluir'],
@@ -504,48 +412,35 @@ ipcMain.on('delete-task', async (event, args) => {
         message: 'Tem certeza que deseja excluir este cliente?'
     })
     console.log(response)
-    
-    // Apoio a Logica
 
-    //passo 3 excluir a tarefa do banco e enviar uma resposta para o renderer
     if (response === 1) {
         const cadastroExcluido = await Cadastros.findByIdAndDelete(args)
-        //passo 3 excluir a tarefa do banco e enviar uma resposta para o renderer atualizar a lista de tarefas pendentes
         event.reply('delete-task-success', JSON.stringify(cadastroExcluido))
 
     }
 })
 
-//Buscar um cliente especifico no banco de dados (Crud read)
-//passo 3 recebimento do nome do cliente
 ipcMain.on('search-client', async (event, nome) => {
-    console.log(nome) //teste do passo 3
-    //passo 4 - buscar o nome no banco de dados
+    console.log(nome)
     try {
         const dadosCliente = await Cadastros.find({
-            nome: new RegExp(nome, 'i') //i ignore(letras maiuscula/minuscula)    
+            nome: new RegExp(nome, 'i')   
         })
-        console.log(dadosCliente) //teste passo 4
-        //validação (se não existir o cliente informar o usuario)
+        console.log(dadosCliente)
         if (dadosCliente.length === 0) {
             dialog.showMessageBox(winAbout, {
                 type: 'question',
                 message: 'Cliente não cadastrado.\nDeseja cadastrar este cliente?',
                 buttons: ['Sim', 'Não']
             }).then((result) => {
-                //verifica o botão pressionado (Sim = 0)
-                // console.log(result)
                 if (result.response === 0) {
-                    //setar o nome na caixa input
                     event.reply('set-name')
                 } else {
-                    //limpar a caixa input de busca
                     event.reply('clear-search')
                 }
             })
 
         } else {
-            // se existir o cliente pesquisado, enviar os dados para o renderer (passo 5)
             event.reply('client-data', JSON.stringify(dadosCliente))
         }
     } catch (error) {
@@ -562,36 +457,28 @@ ipcMain.on('search-alert', (event) => {
     event.reply('search-focus')
 })
 
-//------------------------pesquisar produto------------------------------------------------------------
-
 ipcMain.on('search-produto', async (event, codigo) => {
-    console.log(codigo) //teste do passo 3
-    //passo 4 - buscar o nome no banco de dados
+    console.log(codigo)
     try {
       const dadosProdutos = await Produto.find({
-        codigo: new RegExp(codigo, 'i') //i ignore(letras maiuscula/minuscula)    
+        codigo: new RegExp(codigo, 'i')    
       })
-      console.log(dadosProdutos) //teste passo 4
-      //validação (se não existir o cliente informar o usuario)
+      console.log(dadosProdutos)
       if (dadosProdutos.length === 0) {
         dialog.showMessageBox(produtos, {
           type: 'question',
           message: 'Produto não cadastrado.\nDeseja cadastrar este Produto?',
           buttons: ['Sim', 'Não']
         }).then((result) => {
-          //verifica o botão pressionado (Sim = 0)
-          // console.log(result)
+
           if (result.response === 0) {
-            //setar o nome na caixa input
             event.reply('set-produto')
           } else {
-            //limpar a caixa input de busca
             event.reply('clear-search')
           }
         })
   
       } else {
-        // se existir o cliente pesquisado, enviar os dados para o renderer (passo 5)
         event.reply('produto-data', JSON.stringify(dadosProdutos))
       }
     } catch (error) {
@@ -599,13 +486,8 @@ ipcMain.on('search-produto', async (event, codigo) => {
     }
   })
 
-//Editar um cliente  - CRUD Update 
-
-//CRUD Update >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
-//Passo 3(slide) - receber o pedido do renderer para editar a tarefa no banco de dados
 ipcMain.on('update-task', async (event, cliente) => {
-    console.log(cliente) //teste de recebimento dos dados do form
-    //validação de campos obrigatórios
+    console.log(cliente)
     if (cliente.nome === "") {
         dialog.showMessageBox(winAbout, {
             type: "info",
@@ -619,7 +501,6 @@ ipcMain.on('update-task', async (event, cliente) => {
             buttons: ['OK']
         })
     } else {
-        //Passo 4 slide - Alterar as informações no banco de dados
         const clienteEditado = await Cadastros.findByIdAndUpdate(
             cliente.id, {
             nome: cliente.nome,
@@ -639,25 +520,21 @@ ipcMain.on('update-task', async (event, cliente) => {
                 new: true
             }
         )
-        //enviar a confirmação para o renderer junto com a tarefa editada (passo 5 do slide)
         dialog.showMessageBox(winAbout, {
             type: 'info',
             message: "Dados do Cliente alterado com sucesso",
             buttons: ['OK']
         }).then((result) => {
-            //verificar se o botão ok foi preenchido
             if (result.response === 0) {
                 event.reply('update-success', JSON.stringify(clienteEditado))
             } else {
-                //limpar a caixa de busca 
                 event.reply('clear-search')
             }
         })
     }})
 
 ipcMain.on('update-client', async (event, cliente) => {
-    await console.log(cliente) // teste de recebimento do renderer
-    //passo 3: salvar as alterações no banco de dados 17/04/2024
+    await console.log(cliente)
     const clienteEditado = await Cadastros.findByIdAndUpdate(
         cliente.id, {
         nome: cliente.nome,
