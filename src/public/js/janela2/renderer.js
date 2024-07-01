ddf5c527f782a87e424d485837e292e07ef3e48c
Andres Cristian <cristian.yapiticona@gmail.com>
2024-06-30 21:00:15 -0300

    Update renderer.js

--- src/public/js/janela2/renderer.js
+++ src/public/js/janela2/renderer.js
@@ -1,21 +1,14 @@
-const {ipcRenderer} = require('electron')
-
+const { ipcRenderer } = require('electron')
 ipcRenderer.send('send-message', "Status do bando de dados:")
 
-
-//receber mensagens do processo principal sobre o status da conexão
 ipcRenderer.on('db-status', (event, status) => {
     console.log(status)
-    if(status === "Banco de dados conectado") {
+    if (status === "Banco de dados conectado") {
         
-    }else{
+    } else {
         
     }
 })
-
-
-//CRUD CREATE - Inserir dados na tabela
-//Passo 1 - (Receber os dados do form)
 
 let  formulario, formNome, formTelefone, formEmail, formCep, formLogradouro, formBairro, formCidade, formUf, formCpf, lista
 formulario = document.querySelector("#formCadastros")
@@ -31,18 +24,11 @@ formUf = document.querySelector("#formUf")
 lista = document.querySelector("#listaCadastros")
 
 let arrayCadastros = []
-
 let updateStatus = false
 let idCadastro
 
-
-//Recebimento dos dados do formulario ao precionar o botão salvar - passo 1 do slide
 formulario.addEventListener("submit", async (event) =>{
-    event.preventDefault()//Ignorar o comportamento padrão (reiniciar o documento após envio dos dados do formulario)
-    //console.log("Recebendo")
-    //console.log(formNome.value, formTelefone.value, formEmail.value)
-    //passo 1 do slide - envio do dados para o main 
-    //criar uma estrutura de dados usando objeto para enviar ao main (argumentos)
+    event.preventDefault()
     const cadastros = {
         nome: formNome.value, 
         telefone: formTelefone.value,
@@ -53,61 +39,34 @@ formulario.addEventListener("submit", async (event) =>{
         bairro: formBairro.value,
         cidade: formCidade.value,
         uf: formUf.value
-
     }
+    
     if (updateStatus === false) {
-        ipcRenderer.send('new-task', cadastros,) // passo 2 do slide crud create- envio dos dados para o main 
-
-    }else {
+        ipcRenderer.send('new-task', cadastros)
+    } else {
         ipcRenderer.send('update-task', {...cadastros, idCadastro})
     }
-
     
     formulario.reset()
     renderizarCadastros(arrayCadastros)
-
 })
 
-
-
-//confirmar cadastro da tarefas no  banco  de dados
-ipcRenderer.on('new-task-created', (event,args) =>{
-    //CRUD READ - Passo extra:atualizar a lista automaticamente quando uma nova tarefa for adicionada ao banco 
+ipcRenderer.on('new-task-created', (event,args) => {
     const novoCadastros = JSON.parse(args)
     arrayCadastros.push(novoCadastros)
     renderizarCadastros(arrayCadastros)
-    
 })
 
-//>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
-//Copia do de cima 
-
-//>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
-//CRUD READ - Buscar os dados do banco
-//Enviar para o main o pedido para buscar as tarefas pendentes no banco de dados (passo 1 slide)
 ipcRenderer.send('get-tasks')
-//Passo 3 (slide) receber as 
 ipcRenderer.on('pending-tasks', (event, args) => {
-    console.log(args)//passo 3 - fins didaticos teste de recebimento das tarefas pendentes
-    //Passo 4 -  Renderizar as tarefas pendentes no documento index html
-    /**
-     *  4.1 criar uma lista ou tabela no html 
-     *  4.2 Capturar o id a lista ou tabela
-     *  4.3 criar um vetor para estruturar os dados
-     *  4.4 Criar uma função para renderizar a lista ou tabela
-    */
-
-    //Criar uma constante para receber as tarefas pendentes
-    //JSON.parse (Garantir o formato JSON)
+    console.log(args)
     const cadastrosPendentes = JSON.parse(args)
-    //Atribuir ao vetor
     arrayCadastros = cadastrosPendentes
-    console.log(arrayCadastros) //fins didaticos - exibir a estrutura de dados
-    //Executar a função renderizarTarefas() passando com parâmetro o array
+    console.log(arrayCadastros)
     renderizarCadastros(arrayCadastros)
 })
 
-function editarCadastro(id){
+function editarCadastro(id) {
     console.log(id)
     updateStatus = true
     idCadastro = id
@@ -121,18 +80,12 @@ function editarCadastro(id){
     formBairro.value = cadastroEditado.bairro
     formCidade.value = cadastroEditado.cidade
     formUf.value = cadastroEditado.uf
-
 }
 
-
-
-//passo 5 e 6 crud update - receber a confirmação do update e renderizar novamente
 ipcRenderer.on('update-task-success', (event, args) =>{
-    console.log(args) // teste do passo 5 (recebimento do main )
-    //renderizar a tarefa - passo 6 (mapeamento do array)
+    console.log(args)
     const cadastroEditado= JSON.parse(args)
     arraycadastroEditado = arrayCadastros.map(t => {
-        //se o id  for igual a cadastros editada
         if(t._id === cadastroEditado._id) {
             t.nome = cadastroEditado.nome
             t.telefone = cadastroEditado.telefone
@@ -143,39 +96,20 @@ ipcRenderer.on('update-task-success', (event, args) =>{
             t.bairro = cadastroEditado.bairro
             t.cidade = cadastroEditado.cidade
             t.uf = cadastroEditado.uf
-
         }
         return t
     })
     renderizarCadastros(arraycadastroEditado)
-    updateStatus = false // sinaliza o fim do update 
+    updateStatus = false
 })
 
-//>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
-//CRUD Delete - excluir os dados do banco
-/**
-  passo 1
-  1.1 - Criar o botão excluir na lista de tarefas
-  1.2 - criar a função excluirTarefa e testar no console
-  1.3 - testar a passagem do id como parâmetro 
-  passo 2 - confirma a exclusão
-  passo 3 excluir a tarefa do banco e enviar uma resposta para o renderer
-  passo 3 excluir a tarefa do banco e enviar uma resposta para o renderer atualizar a lista de tarefas pendentes
- */
-
-  //passo 1.2 Crud delete
-    function excluirCadastro(id){
-        console.log(id)//passo 1.3 crud delete
-        //passo 2 - confirma a exclusão(main) -> enviar este ao main junto com o id da tarefa a ser excluida
+function excluirCadastro(id){
+    console.log(id)
     ipcRenderer.send('delete-task', id)
-    }
+}
 
-    
-
-//passo 4 crud delete - receber a confirmação de exclusão e 
 ipcRenderer.on('delete-task-success', (event, args) =>{
     console.log(args)
-    //atualizar a lista de tarefas pendentes usando um filtro no array para remover a tarefa excluida
     const cadastroEliminado = JSON.parse(args)
     const cadastroAtualizado = arrayCadastros.filter((t)=> {
         return t._id !== cadastroEliminado._id
@@ -184,39 +118,31 @@ ipcRenderer.on('delete-task-success', (event, args) =>{
     renderizarCadastros(arrayCadastros)
 })
 
-
-//passo 1.1 criar o botão crud Delete
-//Passo 4 - Função usada para renderizar(exibir) os dados em uma lista ou tabela usando a linguagem html 
 function renderizarCadastros(tasks) {
-
     tasks.sort((a,b) => {
         const nomeA = a.nome.toLowerCase();
         const nomeB = b.nome.toLowerCase();
-
-    if (nomeA < nomeB) return -1;
-    if (nomeA > nomeB) return 1;
-    return 0;
+        if (nomeA < nomeB) return -1;
+        if (nomeA > nomeB) return 1;
+        return 0;
     });
-lista.innerHTML="" //Limpar a lista
-//percorrer o array
-tasks.forEach((t) => {
-lista.innerHTML += `
-
-<tr>    
-<td>${t.nome}</td>
-<td>${t.telefone}</td>
-<td>${t.email}</td>
-<td>${t.cpf}</td>
-<td>${t.cep}</td>
-<td>${t.logradouro}</td>
-<td>${t.numero}</td>
-<td>${t.complemento}</td>
-<td>${t.bairro}</td>
-<td>${t.cidade}</td>
-<td>${t.uf}</td>
-</tr>
-`  
-})
-
+    
+    lista.innerHTML=""
+    tasks.forEach((t) => {
+    lista.innerHTML += `
+    <tr>    
+    <td>${t.nome}</td>
+    <td>${t.telefone}</td>
+    <td>${t.email}</td>
+    <td>${t.cpf}</td>
+    <td>${t.cep}</td>
+    <td>${t.logradouro}</td>
+    <td>${t.numero}</td>
+    <td>${t.complemento}</td>
+    <td>${t.bairro}</td>
+    <td>${t.cidade}</td>
+    <td>${t.uf}</td>
+    </tr>
+    `
+    })
 }
-
