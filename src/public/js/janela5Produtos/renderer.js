e6a6f7fe24052842e122efa103fba3a6d647b852
Andres Cristian <cristian.yapiticona@gmail.com>
2024-06-30 21:19:38 -0300

    Update renderer.js

--- src/public/js/janela5Produtos/renderer.js
+++ src/public/js/janela5Produtos/renderer.js
@@ -1,15 +1,11 @@
 const { ipcRenderer } = require('electron')
-
 ipcRenderer.send('send-message', "Status do bando de dados:")
 
-
-//Modificando propriedades dos elementos HTML ao iniciar o documento 
 document.addEventListener("DOMContentLoaded", () => {
-    btnUpdate.disabled = true //desativar botão
-    btnDelete.disabled = true //desativar botão
+    btnUpdate.disabled = true
+    btnDelete.disabled = true
 })
 
-// Botão clean (Limpar campos e setar os botões)
 function clean() {
     document.getElementById("btnSalvar").disabled = false
     document.getElementById("btnUpdate").disabled = true
@@ -20,12 +16,9 @@ function clean() {
     document.getElementById("inputProduct").focus()
 }
 
-
-
-//CRUD CREATE - Inserir dados na tabela
-//Passo 1 - (Receber os dados do form)
-
-let formulario, formNome, formTelefone, formEmail, formCep, formLogradouro, formBairro, formCidade, formUf, formCpf, formNumero, formComplemento, lista, idCliente, formCnpj, formEmpresa, formInscricao, formContato, formSite, formCodigo, formProduto, formEstoque, formValor, nomeFornecedor, codigoDeBarra, lucro, resultado, preco
+let formulario, formNome, formTelefone, formEmail, formCep, formLogradouro, formBairro, formCidade, formUf, formCpf, 
+formNumero, formComplemento, lista, idCliente, formCnpj, formEmpresa, formInscricao, formContato, formSite,
+formCodigo, formProduto, formEstoque, formValor, nomeFornecedor, codigoDeBarra, lucro, resultado, preco
 formulario = document.querySelector("#formCadastros")
 idCliente = document.querySelector('#idClient')
 formTelefone = document.querySelector("#formTelefone")
@@ -60,11 +53,9 @@ imagemProdutoInput = document.querySelector("#imagemProduto")
 //renderizar imagem
 imagemProdutoPreview = document.querySelector("#imagemProdutoPreview")
 
-
 let arrayProdutos = []
 let arrayFornecedores = []
 let arrayFornecedor = []
-
 let updateStatus = false
 let idCadastro
 
@@ -75,17 +66,10 @@ ipcRenderer.on('manda-nome', (event, args) => {
     let nomeFornecedores = JSON.parse(args)
     arrayFornecedores = nomeFornecedores
     renderizarFornecedores(arrayFornecedores)
-
-
 })
 
-// //Recebimento dos dados do formulario ao precionar o botão salvar - passo 1 do slide
 formulario.addEventListener("submit", async (event) => {
-    event.preventDefault()//Ignorar o comportamento padrão (reiniciar o documento após envio dos dados do formulario)
-    //console.log("Recebendo")
-    //console.log(formNome.value, formTelefone.value, formEmail.value)
-    //passo 1 do slide - envio do dados para o main 
-    //criar uma estrutura de dados usando objeto para enviar ao main (argumentos)
+    event.preventDefault()
     const cadastros = {
         empresa: nomeFornecedor.value,
         codigo: formCodigo.value,
@@ -104,18 +88,11 @@ formulario.addEventListener("submit", async (event) => {
     } else {
         ipcRenderer.send('update-produto', { ...cadastros, idCadastro })
     }
-
-
-
     renderizarCadastros(arrayFornecedor)
     renderizarFornecedores(arrayFornecedor)
 })
 
-
-
-//confirmar cadastro da tarefas no  banco  de dados
 ipcRenderer.on('new-produto-created', async (event, args) => {
-    //CRUD READ - Passo extra:atualizar a lista automaticamente quando uma nova tarefa for adicionada ao banco 
     console.log(args)
     const novoProduto = JSON.parse(args)
     console.log(novoProduto)
@@ -148,118 +125,32 @@ function relatorioProdutos() {
     ipcRenderer.send('relatorio-produtos')
 }
 
-
-//>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
-//Copia do de cima 
-
-//>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
-//CRUD READ - Buscar os dados do banco
-//Enviar para o main o pedido para buscar as tarefas pendentes no banco de dados (passo 1 slide)
 ipcRenderer.send('get-produto')
-//Passo 3 (slide) receber as 
+
 ipcRenderer.on('pending-produto', (event, args) => {
-    console.log(args)//passo 3 - fins didaticos teste de recebimento das tarefas pendentes
-    //Passo 4 -  Renderizar as tarefas pendentes no documento index html
-    /**
-     *  4.1 criar uma lista ou tabela no html 
-     *  4.2 Capturar o id a lista ou tabela
-     *  4.3 criar um vetor para estruturar os dados
-     *  4.4 Criar uma função para renderizar a lista ou tabela
-    */
-
-    //Criar uma constante para receber as tarefas pendentes
-    //JSON.parse (Garantir o formato JSON)
+    console.log(args)
     const fornecedorPendentes = JSON.parse(args)
-    //Atribuir ao vetor
     arrayFornecedor = fornecedorPendentes
-    console.log(arrayFornecedor) //fins didaticos - exibir a estrutura de dados
-    //Executar a função renderizarTarefas() passando com parâmetro o array
+    console.log(arrayFornecedor)
     renderizarCadastros(arrayFornecedor)
-
-
 })
 
 document.getElementById("inputProduct").addEventListener("keydown", function (event) {
     if (event.key === "Enter") {
-        event.preventDefault() // Evita o envio do formulário padrão 
+        event.preventDefault()
         pesquisarProduto()             
         codigo = codigoDeBarra.value
         document.getElementById("inputProduct").disabled = true
     }
-
 })
 
-// function editarCadastro(id){
-//     console.log(id)
-//     updateStatus = true
-//     idCadastro = id
-//     const cadastroEditado = arrayCadastros.find(arrayCadastros => arrayCadastros._id === id)
-//     formNome.value = cadastroEditado.nome
-//     formTelefone.value = cadastroEditado.telefone
-//     formEmail.value = cadastroEditado.email
-//     formCpf.value = cadastroEditado.cpf
-//     formCep.value = cadastroEditado.cep
-//     formLogradouro.value = cadastroEditado.logradouro
-//     formNumero.value = cadastroEditado.numero
-//     formComplemento.value = cadastroEditado.complemento
-//     formBairro.value = cadastroEditado.bairro
-//     formCidade.value = cadastroEditado.cidade
-//     formUf.value = cadastroEditado.uf
-
-// }
-
-//passo 5 e 6 crud update - receber a confirmação do update e renderizar novamente
-// ipcRenderer.on('update-task-success', (event, args) =>{
-//     console.log(args) // teste do passo 5 (recebimento do main )
-//     //renderizar a tarefa - passo 6 (mapeamento do array)
-//     const cadastroEditado= JSON.parse(args)
-//     arraycadastroEditado = arrayCadastros.map(t => {
-//         //se o id  for igual a cadastros editada
-//         if(t._id === cadastroEditado._id) {
-//             t.nome = cadastroEditado.nome
-//             t.telefone = cadastroEditado.telefone
-//             t.email = cadastroEditado.email
-//             t.cpf = cadastroEditado.cpf
-//             t.cep = cadastroEditado.cep
-//             t.logradouro = cadastroEditado.logradouro
-//             t.numero = cadastroEditado.numero
-//             t.complemento = cadastroEditado.complemento
-//             t.bairro = cadastroEditado.bairro
-//             t.cidade = cadastroEditado.cidade
-//             t.uf = cadastroEditado.uf
-
-//         }
-//         return t
-//     })
-//     renderizarCadastros(arraycadastroEditado)
-//     updateStatus = false // sinaliza o fim do update 
-// })
-
-//>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
-//CRUD Delete - excluir os dados do banco
-/**
-  passo 1
-  1.1 - Criar o botão excluir na lista de tarefas
-  1.2 - criar a função excluirTarefa e testar no console
-  1.3 - testar a passagem do id como parâmetro 
-  passo 2 - confirma a exclusão
-  passo 3 excluir a tarefa do banco e enviar uma resposta para o renderer
-  passo 3 excluir a tarefa do banco e enviar uma resposta para o renderer atualizar a lista de tarefas pendentes
- */
-
-//passo 1.2 Crud delete
 function excluirCadastro(id) {
-    console.log(id)//passo 1.3 crud delete
-    //passo 2 - confirma a exclusão(main) -> enviar este ao main junto com o id da tarefa a ser excluida
+    console.log(id)
     ipcRenderer.send('delete-task', id)
 }
 
-
-
-//passo 4 crud delete - receber a confirmação de exclusão e 
 ipcRenderer.on('delete-task-success', (event, args) => {
     console.log(args)
-    //atualizar a lista de tarefas pendentes usando um filtro no array para remover a tarefa excluida
     const cadastroEliminado = JSON.parse(args)
     const cadastroAtualizado = arrayProdutos.filter((t) => {
         return t._id !== cadastroEliminado._id
@@ -268,22 +159,9 @@ ipcRenderer.on('delete-task-success', (event, args) => {
     renderizarCadastros(arrayProdutos)
 })
 
-//passo 1.1 criar o botão crud Delete
-//Passo 4 - Função usada para renderizar(exibir) os dados em uma lista ou tabela usando a linguagem html 
 function renderizarCadastros(tasks) {
-
-    // tasks.sort((a,b) => {
-    //     const nomeA = a.empresa.toLowerCase();
-    //     const nomeB = b.empresa.toLowerCase();
-    
-    // if (nomeA < nomeB) return -1;
-    // if (nomeA > nomeB) return 1;
-    // return 0;
-    // });
-    //percorrer o array
     tasks.forEach((t) => {
-        lista.innerHTML += `
-    
+    lista.innerHTML += `
     <tr>    
     <td>${t.empresa}</td>
     <td>${t.codigo}</td>
@@ -293,35 +171,25 @@ function renderizarCadastros(tasks) {
     <td>${t.lucro}%</td>
     <td>R$${t.resultado}</td>
     <td>R$ ${t.estoque * t.valor}</td>
-    
-
-
     </tr>
     `
     })
-
 }
 
 function renderizarFornecedores(fornecedores) {
     fornecedores.forEach((t) => {
         console.log(t.empresa)
         nomeFornecedor.innerHTML += `
-
-        
         /<option value="${t.empresa}">${t.empresa}</option>
         `
     })
 }
 
-//----------------------------------pesquisar cliente pelo nome CRUD READ-------------------------------------------
 function pesquisarProduto() {
-    //PASSO 1 - RECEBER O NOME DO CLIENTE PARA BUSCA NO BANCO
     let produto = document.getElementById('inputProduct').value
-    //VALIDAÇAO DO CAMPO DE BUSCA (PREENCHIMENTO OBRIGATÓRIO)
     if (produto === '') {
         ipcRenderer.send('search-alert')
     } else {
-        //PASSO 2 - ENVIAR PARA O MAIN O NOME DO CLIENTE
         ipcRenderer.send('search-produto', produto)
     }
 }
@@ -330,18 +198,13 @@ ipcRenderer.on('sourch-focus', () => {
     document.getElementById('inputSearch').focus
 })
 
-
 let arrayProduto = []
 
-//receber os dados do cliente (se existir o cadastro) - passo 6
 ipcRenderer.on('produto-data', (event, dadosProdutos) => {
-    console.log(dadosProdutos) // teste do passo 6
-
-    //passo 7 manipular a estrutura de dados e renderizar o documento html preenchendo as caixas de texto(input) com os dados extraidos do array
+    console.log(dadosProdutos)
     const produto = JSON.parse(dadosProdutos)
     arrayProduto = produto
-    console.log(arrayProduto)//apoio a logica (extração dos dados  do array )
-    //percorrer o array e setar as caixas input (caixas de texto)
+    console.log(arrayProduto)
     arrayProduto.forEach((c) => {
         document.getElementById("idProduct").value = c._id
         document.getElementById("idClient").value = c._id
@@ -351,21 +214,17 @@ ipcRenderer.on('produto-data', (event, dadosProdutos) => {
         document.getElementById("formValor").value = c.valor
         document.getElementById("formFornecedor").value = c.empresa
         document.getElementById("resultado").value = c.resultado
-        // ###################################################
+
         // renderizar imagem
         imagemProdutoPreview.src = c.imagemProduto
 
-        //limpar a caixa de texto de pesquisa
         document.getElementById("inputProduct").value = ""
         document.getElementById("inputProduct").disabled = false
-        //desativar o botão Adicionar
         document.getElementById("btnSalvar").disabled = true
-        //ativar os botões editar e excluir
         document.getElementById("btnUpdate").disabled = false
         document.getElementById("btnDelete").disabled = false
     })
 })
-
 
 ipcRenderer.on('set-produto', () => {
     let setarProduto = document.getElementById("inputProduct").value
@@ -373,14 +232,12 @@ ipcRenderer.on('set-produto', () => {
     document.getElementById("inputSearch").value = ""
 })
 
-//limpar a caixa de busca caso o usuário não queira cadastrar um novo cliente
 ipcRenderer.on('clear-search', () => {
     document.getElementById("inputSearch").value = ""
 })
 
 
 function editarProduto() {
-    //passo 1 - capturar os dados das caixas de texto 17/04/2024
     const produto = {
         id: idCliente.value,
         empresa: nomeFornecedor.value,
@@ -390,27 +247,17 @@ function editarProduto() {
         valor: formValor.value,
         lucro: lucro.value,
         resultado: resultado.value
-    
-        // formCodigo, formProduto, formEstoque, formValor
     }
-    console.log(produto) // teste da captura dos dados
-    //passo 2 - enviar a estrutura de dados para o main 
+    console.log(produto)
     ipcRenderer.send('update-produto', produto)
-    
-    
 }
 
-//passo 5 : limpar os campos e setar os botões
 ipcRenderer.on('udpate-produto-success', () => {
     formulario.reset()
     clean()
 })
 
-
-
-
 function excluirProduto() {
-    //passo 1 - capturar os dados das caixas de texto 17/04/2024
     const produto = {
         id: idCliente.value,
         empresa: nomeFornecedor.value,
@@ -420,22 +267,16 @@ function excluirProduto() {
         valor: formValor.value,
         lucro: lucro.value,
         resultado: resultado.value
-
-
     }
-    console.log(produto) // teste da captura dos dados
-    //passo 2 - enviar a estrutura de dados para o main 
+    console.log(produto)
     ipcRenderer.send('excluir-produto', produto)
 }
 
-//passo 5 : limpar os campos e setar os botões
 ipcRenderer.on('excluir-success-produto', () => {
     formulario.reset()
     clean()
 })
 
-
-//receber mensagens do processo principal sobre o status da conexão
 ipcRenderer.on('db-status', (event, status) => {
     console.log(status)
     if (status === "Banco de dados conectado") {
@@ -448,33 +289,7 @@ ipcRenderer.on('db-status', (event, status) => {
 let url = document.getElementById("formSite").value
 console.log(url)
 
-
-
-//botão visitar site 
-//document.getElementById('myButton').addEventListener('click', () => {
-//shell.openExternal(url)
-//})
-
 function entrarsite() {
     let url = document.getElementById("formSite").value
-
-
     ipcRenderer.send('site-url', url)
 }
-
-//codigo de barra
-
-// CRUD Read >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
-// BarCode //codigo de barra 10/05/2024
-// const inputField = document.getElementById("inputProduct");
-// let inputValue = ""
-
-// document.getElementById("inputProduct").addEventListener("keydown", function (event) {
-//     if (event.key === "Enter") {
-//         event.preventDefault() // Evita o envio do formulário padrão              
-//         barcode = inputField.value
-//         console.log(barcode)
-//         document.getElementById("inputProduct").disabled = true
-//         ipcRenderer.send('search-barcode', barcode)
-//     }
-// })
